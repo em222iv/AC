433ab3ca971c7249aafa41ec16a26cff2ccb35f4
em222iv <erikper.magnusson@gmail.com>
2014-11-20 11:20:03 +0100

    annotations

--- AdventurousContacts/Models/Contact.cs
+++ AdventurousContacts/Models/Contact.cs
@@ -11,10 +11,17 @@ namespace AdventurousContacts.Models
     {
         public class Contact_Metadata
         {
-            public int ContactID { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
 
+            [Required]
+            public int ContactID { get; set; }
+            [Required]
+            [MaxLength(50, ErrorMessage="No more than 50 characters")]
+            public string FirstName { get; set; }
+            [Required]
+            [MaxLength(50, ErrorMessage = "No more than 50 characters")]
+            public string LastName { get; set; }
+            [Required]
+            [DataType(DataType.EmailAddress,ErrorMessage="Not a valid email format")]
             public string EmailAddress { get; set; }
         }
     }
